13442ee075cd35afe039316e16db30236a50a7bd
meloshnikov <meloshnikoff@gmail.com>
2025-01-07 14:24:05 +0300

    Добавляет тесты на стек.

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,37 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def setup
+    @stack = Stack.new
+    @element = 1
+  end
+
+  def teardown
+    @stack = nil
+  end
+
+  def test_add_element
+    @stack.push!(@element)
+    assert_equal @stack.to_a[0], @element
+  end
+
+  def test_remove_element
+    @stack.push!(@element)
+    @stack.pop!
+    assert_empty @stack.to_a
+  end
+
+  def test_clear
+    @stack.push!(@element)
+    @stack.clear!
+    assert @stack.empty?
+    assert_empty @stack.to_a
+  end
+
+  def test_empty_check
+    assert @stack.empty?
+    assert_empty @stack.to_a
+  end
   # END
 end
 
