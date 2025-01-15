aea1cbc6e5355644e54d13ad2ee56c983d9c9f1a
@hexlet/cli <support@hexlet.io>
2025-01-15 22:43:29 +0300

    submit rails-basics-ru/quality

---
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class BulletinsControllerTest < ActionDispatch::IntegrationTest
+  test 'opens all bulletins page' do
+    bulletin1 = bulletins(:bulletin1)
+    bulletin2 = bulletins(:bulletin2)
+    
+    get bulletins_path
+    assert_response :success
+    assert_select 'h1', 'Bulletins'
+    assert_select 'li', bulletin1.title
+    assert_select 'li', bulletin2.title
+  end
+
+  test 'opens one bulletin page' do
+    bulletin1 = bulletins(:bulletin1)
+
+    get bulletin_path(bulletin1)
+    assert_response :success
+    assert_select 'h1', 'Bulletin page'
+    assert_select 'h3', 'Заголовок объявления 1'
+    assert_select 'p', 'Описание объявления 1'
+  end
+end
